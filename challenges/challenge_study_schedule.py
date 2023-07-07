1e5b513643688275db862b436e9bdde2e6017258
wallaC-137 <wallace_031@hotmail.com>
2023-07-07 17:18:26 -0300

    req01: ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,21 @@
+def is_none(value):
+    return None in value
+
+
+def not_int(value):
+    return not all(isinstance(elem, int) for elem in value)
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    frequency = 0
+
+    for i in permanence_period:
+        if is_none(i) or not_int(i):
+            return None
+        if i[0] <= target_time <= i[1]:
+            frequency += 1
+
+    return frequency
