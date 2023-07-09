27653ebff3d60726857d0307e23339eac5b2afe9
wallaC-137 <wallace_031@hotmail.com>
2023-07-09 00:25:41 -0300

    req04: ok / req06: ok

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,37 @@
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left = merge_sort(string[:mid])
+    right = merge_sort(string[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return "".join(merged)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = merge_sort(first_string.lower())
+    second_string = merge_sort(second_string.lower())
+
+    if len(first_string) == 0 or len(second_string) == 0:
+        return (first_string, second_string, False)
+
+    return (first_string, second_string, first_string == second_string)
